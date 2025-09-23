68d873143ddf11ed2db6880e988beaf47200b591
TonyBean <38479405+TonyBean-Dong@users.noreply.github.com>
2025-09-23 16:50:31 +0800

    Update PhysicalConveyorBeltsPartMotion.cs

--- Runtime/Motion/DirectControl/Physical/PhysicalConveyorBeltsPartMotion.cs
+++ Runtime/Motion/DirectControl/Physical/PhysicalConveyorBeltsPartMotion.cs
@@ -22,42 +22,43 @@ namespace NonsensicalKit.DigitalTwin.Motion
 
         [SerializeField] private float m_conversionRate = 1; //转换率，当为1时，数据为0.1代表速度为0.1m/s
 
-        public PhysicalCollisionArea m_Area;
+        [FormerlySerializedAs("m_Area")] [SerializeField]
+        private PhysicalCollisionArea m_area;
 
-       [SerializeField]  private bool m_twoDir = true;
+        [SerializeField] private bool m_twoDir = true;
 
-        private readonly List<PhysicalMaterials> _materials = new();
-        private float _speed1;
-        private float _speed2;
-        private bool _isRunning;
+        protected readonly List<PhysicalMaterials> Materials = new();
+        protected float Speed1;
+        protected float Speed2;
+        protected bool IsRunning;
 
         [Button]
-        private void GetArea()
+        private void GetAreaInChildren()
         {
-            m_Area = GetComponent<PhysicalCollisionArea>();
+            m_area = GetComponentInChildren<PhysicalCollisionArea>();
         }
-        
+
         private void Update()
         {
-            if (_isRunning)
+            if (IsRunning)
             {
-                if (_speed1 == 0 && _speed2 == 0)
+                if (Speed1 == 0 && Speed2 == 0)
                 {
                     return;
                 }
 
                 if (m_twoDir)
                 {
-                    foreach (var item in _materials)
+                    foreach (var item in Materials)
                     {
-                        item.Move(GetDir(m_dir1Type) * _speed1 + GetDir(m_dir2Type) * _speed2);
+                        item.Move(GetDir(m_dir1Type) * Speed1 + GetDir(m_dir2Type) * Speed2);
                     }
                 }
                 else
                 {
-                    foreach (var item in _materials)
+                    foreach (var item in Materials)
                     {
-                        item.Move(GetDir(m_dir1Type) * _speed1);
+                        item.Move(GetDir(m_dir1Type) * Speed1);
                     }
                 }
             }
@@ -67,26 +68,26 @@ namespace NonsensicalKit.DigitalTwin.Motion
         {
             base.Init();
 
-            m_Area.OnMaterialsEnter.AddListener((hpm) => _materials.Add(hpm));
-            m_Area.OnMaterialsExit.AddListener((hpm) => _materials.Remove(hpm));
-            _isRunning = true;
+            m_area.OnMaterialsEnter.AddListener((hpm) => Materials.Add(hpm));
+            m_area.OnMaterialsExit.AddListener((hpm) => Materials.Remove(hpm));
+            IsRunning = true;
         }
 
         protected override void OnReceiveData(List<PointData> part)
         {
             //两个数据，分别代表两个方向的运行速度
-            _speed1 = m_conversionRate * float.Parse(part[0].Value);
+            Speed1 = m_conversionRate * float.Parse(part[0].Value);
             if (m_twoDir)
             {
-                _speed2 = m_conversionRate * float.Parse(part[1].Value);
+                Speed2 = m_conversionRate * float.Parse(part[1].Value);
             }
         }
 
         protected override void Dispose()
         {
             base.Dispose();
-            _isRunning = false;
-            _materials.Clear();
+            IsRunning = false;
+            Materials.Clear();
         }
 
         private Vector3 GetDir(Dir dt)
